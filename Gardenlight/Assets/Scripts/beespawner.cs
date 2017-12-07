32be6f4d2df6ebe84051fa46ad45f04a412562f4
snerkflerks <checkerboardartist@gmail.com>
2017-12-07 07:14:33 -0500

    while true bees

---
+++ Gardenlight/Assets/Scripts/beespawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class beespawner : MonoBehaviour {
+	public GameObject bee;
+	public GameObject player;
+	public int screenWidth = 16;            //Horizontal size fo the screen 
+	public int screenHeight = 10;
+
+	void Start () {
+		StartCoroutine (SpawnBees ());
+	}
+	// Update is called once per frame
+	IEnumerator SpawnBees () {
+		while (true) {
+			yield return new WaitForSeconds (1);
+			Debug.Log ("bees!");
+			Vector2 newPosition = new Vector2 (Random.Range (0, screenWidth), transform.position.y + screenHeight);
+			GameObject currBee = Instantiate (bee, newPosition, Quaternion.identity);
+		}
+	}
+}
